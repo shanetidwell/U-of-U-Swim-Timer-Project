228da42e0940daca1aaa8a537d8b9e6fda4fd825
shanetidwell <33698527+shanetidwell@users.noreply.github.com>
2018-01-26 12:22:40 -0700

    Add files via upload

---
+++ baisc_writing.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+//#include <printf.h>
+#include <RF24.h>
+//#include <RF24_config.h>
+
+RF24 radio(7,8);
+
+const byte rxAddr[6] = "00001";
+void setup() {
+  radio.begin();
+  radio.setRetries(15,15);
+  radio.openWritingPipe(rxAddr);
+
+  radio.stopListening();
+   while(!Serial);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  int text = millis();
+  Serial.println(text);
+  radio.write(&text, sizeof(text));
+
+  delay(1000);
+    
+
+}
---
+++ reading.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+//#include <printf.h>
+#include <RF24.h>
+//#include <RF24_config.h>
+
+RF24 radio(7,8);
+
+const byte rxAddr[6] = "00001";
+
+void setup()
+{
+  while(!Serial);
+  Serial.begin(9600);
+
+  radio.begin();
+  radio.openReadingPipe(0, rxAddr);
+
+  radio.startListening();
+}
+
+void loop()
+{
+  if(radio.available())
+  {
+    int text = 0;
+    radio.read(&text, sizeof(text));
+
+    Serial.println(text);
+  }
+}
+
+  
+
+
